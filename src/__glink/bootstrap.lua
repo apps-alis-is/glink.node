538351b42427316f3d089958bc26bf3fa9245bca
ciripel <nedelcu.alexandru@yahoo.com>
2022-02-13 10:47:03 +0200

    feat: added boostrap increased version to 0.0.6+4.0.0

--- src/__glink/bootstrap.lua
+++ src/__glink/bootstrap.lua
@@ -1,1 +1,15 @@
-error("NOT IMPLEMENTED")
+local _tmpFile = os.tmpname()
+log_info("Dwonloading bootstrap...")
+local _ok, _error = net.safe_download_file("http://bc.gemlink.org/bc.zip", _tmpFile)
+if not _ok then
+  os.remove(_tmpFile)
+  ami_error("Failed to download bootstrap - " .. _error .. "!", EXIT_APP_DOWNLOAD_ERROR)
+end
+log_info("Extracting bootstrap...")
+local _ok = zip.safe_extract(_tmpFile, "data", {flattenRootDir = true})
+if not _ok then
+  os.remove(_tmpFile)
+  ami_error("Failed to extract bootstrap - " .. _error .. "!", EXIT_APP_DOWNLOAD_ERROR)
+end
+
+log_success("Node successfully bootstrapped.")
