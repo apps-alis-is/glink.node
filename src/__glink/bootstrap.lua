2e363b9728ff0fb711e96c7032bd433528e44e34
ciripel <nedelcu.alexandru@yahoo.com>
2022-02-13 11:49:50 +0200

    feat: implemented progress percentage - implemented progress percentage for bootstrap downloading

--- src/__glink/bootstrap.lua
+++ src/__glink/bootstrap.lua
@@ -1,6 +1,17 @@
 local _tmpFile = os.tmpname()
 log_info("Dwonloading bootstrap...")
-local _ok, _error = net.safe_download_file("http://bc.gemlink.org/bc.zip", _tmpFile)
+local _ok, _error = net.safe_download_file("http://bc.gemlink.org/bc.zip", { progressFunction = (function ()
+    local _lastWritten = 0
+    return function(total, current) 
+        local _progress = math.floor(current / total * 100)
+        if math.fmod(_progress, 10) == 0 and _lastWritten ~= _progress then 
+            _lastWritten = _progress
+            io.write(_progress .. "%...")
+            io.flush()
+            if _progress == 100 then print() end
+        end
+    end
+end)()}, _tmpFile)
 if not _ok then
   os.remove(_tmpFile)
   ami_error("Failed to download bootstrap - " .. _error .. "!", EXIT_APP_DOWNLOAD_ERROR)
