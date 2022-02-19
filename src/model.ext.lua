df802c954a2e98c4ef767df07942553bc40ab92f
ciripel <37701673+ciripel@users.noreply.github.com>
2022-02-19 14:32:10 +0200

    fix: increased version to 0.0.9+4.0.4 (#5)
    * fix: increased version to 0.0.9+4.0.4
    - updated daemon to 4.0.4

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,9 +1,13 @@
 am.app.set_model(
     {
-        DAEMON_URL = "https://github.com/gemlink/gemlink/releases/download/v4.0.2/gemlink-ubuntu-4.0.2.zip",
+        DAEMON_URL = "https://github.com/gemlink/gemlink/releases/download/v4.0.4/gemlink-ubuntu-4.0.4.zip",
         DAEMON_CONFIGURATION = {
-            server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
-            listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
+            server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and
+                1 or
+                nil,
+            listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and
+                1 or
+                nil,
             masternodeprivkey = am.app.get_configuration("NODE_PRIVKEY"),
             masternode = am.app.get_configuration("NODE_PRIVKEY") and 1 or nil
         },
@@ -13,5 +17,5 @@ am.app.set_model(
         CONF_SOURCE = "__btc/assets/daemon.conf",
         SERVICE_NAME = "gemlinkd"
     },
-    { merge = true, overwrite = true }
+    {merge = true, overwrite = true}
 )
