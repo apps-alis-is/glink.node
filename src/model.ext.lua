26a634019dafc2f3123c5913638889cd52953346
ciripel <nedelcu.alexandru@yahoo.com>
2024-03-20 10:51:47 +0200

    update/increase-version-to-0.0.16+4.2.2

--- src/model.ext.lua
+++ src/model.ext.lua
@@ -1,5 +1,5 @@
 am.app.set_model({
-	DAEMON_URL = "https://github.com/gemlink/gemlink/releases/download/v4.2.1/gemlink-ubuntu-4.2.1.zip",
+	DAEMON_URL = "https://github.com/gemlink/gemlink/releases/download/v4.2.2/gemlink-ubuntu-4.2.2.zip",
 	DAEMON_CONFIGURATION = {
 		server = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
 		listen = (type(am.app.get_configuration("NODE_PRIVKEY") == "string") or am.app.get_configuration("SERVER")) and 1 or nil,
@@ -11,6 +11,10 @@ am.app.set_model({
 	CONF_NAME = "gemlink.conf",
 	CONF_SOURCE = "__btc/assets/daemon.conf",
 	SERVICE_NAME = "gemlinkd"
+	ADD_NODES = {
+		15.235.142.201
+		193.25.2.237
+	 }
 },
 	{ merge = true, overwrite = true }
 )
